60dafa8fe2e06f37dd3c82029e0dba9ba8cfa73f
wannabeOG <saihimallu1010@gmail.com>
2019-08-30 18:31:19 +0930

    skeleton code for training the models

--- autoencoder.py
+++ autoencoder.py
@@ -4,23 +4,20 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 
-class autoencoder_expert(nn.Module):
-	def __init__(self, in_features, code_features):
+class Autoencoder(nn.Module):
+	def __init__(self, in_dim, code_dim = 100):
 		super(autoencoder, self).__init__()
 		self.encoder = nn.Sequential(
 			nn.Linear(in_features, code_features),
-			nn.Relu)
-
+			nn.Relu())
 		self.decoder = nn.Linear(code_features, in_features)
 
-	def forward():
-		
+	def forward(self, x):
+		encoded_x = self.encoder(x)
+		reconstructed_x = self.decode(x)
+		return reconstructed_x
 
 
 
 
-
-
-def fill_in_args():
-"""This function is used to fill in the arguments needed to execute the file"""
 
