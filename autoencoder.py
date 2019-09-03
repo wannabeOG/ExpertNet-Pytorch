ccecda42915ebbd7e12d67274b8d666b9cc07cca
wannabeOG <saihimallu1010@gmail.com>
2019-09-03 20:03:56 +0930

    Final changes to the function after testing

--- autoencoder.py
+++ autoencoder.py
@@ -5,20 +5,21 @@ import torch.optim as optim
 
 
 class Autoencoder(nn.Module):
-	def __init__(self, input_dims, code_dims = 100):
-		super(autoencoder, self).__init__()
-		self.encoder = nn.Sequential(
-			nn.Linear(input_dims, code_dims),
-			nn.Relu())
-		self.decoder = nn.Sequential(
-			nn.Linear(code_dims, input_dims),
-			nn.LogSigmoid())
-	
+    def __init__(self, input_dims, code_dims = 100):
+        super(Autoencoder, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(input_dims, code_dims),
+            nn.ReLU())
+        self.decoder = nn.Sequential(
+            nn.Linear(code_dims, input_dims),
+            nn.LogSigmoid())
 
-	def forward(self, x):
-		encoded_x = self.encoder(x)
-		reconstructed_x = self.decode(x)
-		return reconstructed_x
+
+    def forward(self, x):
+        encoded_x = self.encoder(x)
+        reconstructed_x = self.decoder(encoded_x)
+        return reconstructed_x
+
 
 
 
@@ -29,3 +30,21 @@ def encoder_criterion(preds, labels):
 
 
 
+def module_extractor(model):
+	layer_dict = Dict()
+	for name, module in model._modules.item():
+		if (name == "features"):
+			for namex, modulex in module._modules.items():
+				layer_dict[name] = modulex
+
+	return layer_dict
+
+class Alexnet_FE(nn.module):
+	def __init__(self, layer_dict):
+		for key, value in layer_dict:
+			self.add_module(key, value)
+
+	def forward(self, x):
+		for name, module in self._modules.items():
+			x = module(x)
+		return x
