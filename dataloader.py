3b329bd69b999e72437ddaeddf5ce57c18ca5dd2
wannabeOG <saihimallu1010@gmail.com>
2019-09-04 19:53:45 +0930

    Adds the skeleton for the training procedure of the models

---
+++ dataloader.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import warnings
+
+from torch.utils.data import Dataset, Dataloader
+from torchvision import transforms, utils
+
+
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+}
+
+
+
+
--- task_relatedness.py
+++ task_relatedness.py
@@ -6,46 +6,94 @@ from autoencoder import Autoencoder
 def task_metric(rerrror_comp, r_error_ref):
 	return (1-(rerror_ref-r_error_comp)/r_error_comp)
 
-def get_initial_model(dset_loaders, encoder_criterion, use_gpu):
+def kaiming_initilaization(layer):
+	nn.init.kaiming_normal_(m.weight, nonlinearity='sigmoid')
+
+def get_initial_model(feature_extractor, layer_dict, dset_loaders, encoder_criterion, use_gpu):
+	
 	path = os.getcwd()
 	destination = path + "/models/autoencoders"
 	num_ae = len(next(os.walk(destination))[1])
 	best_relatedness = 0
 	model_number = -999
+	device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-	for i in range(num_ae-1):
-		model_path = destination + "/autoencoder_"+str(i+1) +"/best_performing_model.pth"
+	model_path = destination + "/autoencoder_"+str(i+1) +"/best_performing_model.pth"
+	model = Autoencoder(input_dims)
+	model.load_state_dict(torch.load(model_path), map_location= device)
+	
+	for i in range(num_ae):
+		
+		model_path = destination + "/autoencoder_"+str(num_ae-i) +"/best_performing_model.pth"
 		model = Autoencoder(input_dims)
-		model.load_state_dict(torch.load(model_path))
+		model.load_state_dict(torch.load(model_path), map_location= device)
+		
+		feature_extractor = Alexnet_FE(layer_dict)
+		feature_extractor.to(device)
+		
 		model.train(False)
 
 		for data in dset_loaders:
 			
 			input_data, _ = data
-			output = model(input_data)
+			input_data.to(device)
+			input_to_ae = feature_extractor(input_data)
+			input_to_ae = input_to_ae.view(input_to_ae.size(0), -1)
+
+			output = model(input_to_ae)
 			loss = encoder_criterion(outputs, input_data)
-			relatedness = task_metric(loss, rerrror_comp)
-			
-			if (relatedness > best_relatedness):
-				best_relatedness = relatedness
-				model_number = i+1
+			if (i == 0): 
+				rerror_comp = loss
+			else:
+				relatedness = task_metric(loss, rerrror_comp)
+				
+				if (relatedness > best_relatedness):
+					best_relatedness = relatedness
+					model_number = (num_ae - i)
 
 
 	return model_number, best_relatedness			
 
 
-def train_model():
-	model_number, best_relatedness = get_initial_model(dset_loaders, encoder_criterion, use_gpu)
 
-	if (best_relatedness < 0.85):
+def initialize_new_model(model_init, num_classes, num_of_classes_old):
+	weight_info = model_init.classifier[-1].weight.data
+	model_init.classifier[-1] = nn.Linear(model_init.classifier[-1].in_features, num_of_classes_old+num_classes)
+	kaiming_initilaization(model_init.classifier[-1])
+	model_init.classifier[-1].weight[:num_of_classes_old, :] = weight_info
+	return model_init 
+
+
+def train_model(num_classes):
+	model_number, best_relatedness = get_initial_model(feature_extractor, dset_loaders, encoder_criterion, use_gpu)
+	device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") 
+	
+	if (best_relatedness > 0.85):
 
 
 	else:
-		model = models.alexnet(pretrained = False)
-		model.load_state_dict(torch.load(path_to_models + "/model_" + str(model_number) + "best_performing_model.pth"))
+		path_to_dir = path + "/model_" + str(model_number) 
+		
+		file_name = path_to_dir + "/classes.txt" 
+		file_object = open(file_name, 'r')
+		num_of_classes_old = file_object.read()
 
+		model_init = genmodel(num_of_classes_old)
+		model_init.load_state_dict(torch.load(path_to_dir+"/best_performing_model.pth", map_location = device))
+		
+		ref_model = copy.deepcopy(model_init)
+		ref_model.train(False)
+		
+		model_init = initialize_new_model(model_init, num_classes, num_of_classes_old)
+		model_init.to(device)
+
+
+		#Need to freeze weights see how to do that
 		for data in dset_loaders:
 			input_data, labels = data
-						
+
+			with torch.no_grad():
+				ref_labels = ref_model(input_data)
+			
 
 
