51f0193267204b3310689d21aae173be86a093af
wannabeOG <saihimallu1010@gmail.com>
2019-11-07 20:40:24 +0930

    Adds docstrings describing the model classes

--- model_lib.py
+++ model_lib.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+
+"""
+Module containing the realization of all the models that are to be used un t 
+"""
+
 import torch 
 import torch.nn as nn
 import torch.nn.functional as F
@@ -8,7 +13,10 @@ import torch.nn.functional as F
 from torchvision import models
 
 class Autoencoder(nn.Module):
-
+"""
+The class defines the autoencoder model which takes in the features from the last convolutional layer of the 
+Alexnet model. The default value for the input_dims is 256*13*13.
+"""
 	def __init__(self, input_dims = 256*13*13, code_dims = 100):
 		super(Autoencoder, self).__init__()
 		self.encoder = nn.Sequential(
@@ -27,6 +35,10 @@ class Autoencoder(nn.Module):
 
 
 class Alexnet_FE(nn.Module):
+"""
+Create a feature extractor model from an Alexnet architecture, that is used to train the autoencoder model
+and get the most related model whilst training a new task in a sequence
+"""
 	def __init__(self, alexnet_model):
 		super(Alexnet_FE, self).__init__()
 		self.fe_model = nn.Sequential(*list(alexnet_model.children())[0][:-2])
@@ -37,6 +49,10 @@ class Alexnet_FE(nn.Module):
 
 
 class GeneralModelClass(nn.Module):
+	"""
+	Define the model replacing the last linear layer with a linear layer with the required amount of classes
+	for the new task.
+	"""
 	def __init__(self, classes):
 		super(GeneralModelClass, self).__init__()
 		self.Tmodel = models.alexnet(pretrained = True)
